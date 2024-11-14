17b14634889d102185dc94ab2059b77884881b4b
jwoo9 <jwyutong@gmail.com>
2024-11-14 15:17:50 +0000

    working - 11/14/24

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,22 @@
-class SLLTester {
+ class SLLTester {
   public static void main(String[] args) {
+    //create lists
+    SuperLinkedList list1 = new SuperLinkedList();
+    list1.add("a"); list1.add("b"); list1.add("c"); list1.add("d"); list1.add("e");
 
+    SuperLinkedList list2 = new SuperLinkedList();
+    list2.add("e"); list2.add("b"); list2.add("c"); list2.add("b"); list2.add("a");
+
+    SuperLinkedList list3 = new SuperLinkedList();
+    list3.add("A"); list3.add("D"); list3.add("c"); list3.add("C"); list3.add("D"); list3.add("E"); list3.add("B");
+    
+    System.out.println("This is list1");
+    System.out.println(list1);
+    System.out.println("This is list2");
+    System.out.println(list2);
+    System.out.println("This is list3");
+    System.out.println(list3);
+
+    System.out.println(list3.removeDuplicates());
   } // end main
 } // end class
--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -1,18 +1,58 @@
 import java.util.LinkedList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class SuperLinkedList extends LinkedList<String> {
+
+
     public boolean removeVowels() {
-        return false;
-    }
+        ListIterator <String> iter = this.listIterator();
+        int count = 0;
+        while (iter.hasNext()){
+            if ("aeiou".indexOf(iter.next().toLowerCase())!=-1){
+                count ++;
+                iter.remove();
+            } //end if
+        } //end while
+        if (count == 0)
+            return false;
+        return true;
+    } //end method removVowels
 
     public boolean removeConsonants() {
-        return false;
-    }
+        ListIterator <String> iter = this.listIterator();
+        int count = 0;
+        while (iter.hasNext()){
+            if ("aeiou".indexOf(iter.next().toLowerCase())==-1){
+                count ++;
+                iter.remove();
+            } //end if
+        } //end while
+        if (count == 0)
+            return false;
+        return true;
+    } //end method removeConsanants
 
     public LinkedList<String> removeDuplicates() {
-        return null;
-    }
+        LinkedList <String> retList = new LinkedList <String>();
+        ListIterator <String> iter = this.listIterator();
+        for (int i = 0; i<this.size(); i++){
+            iter=this.listIterator();
+            boolean found = false;
+            String search = this.get(i);
+            while (iter.hasNext()){
+                if (iter.next().equals(search)){
+                    if (!found)
+                        found = true;
+                    else{
+                        retList.add(iter.previous());
+                        iter.remove();
+                    }//end inner else
+                } //end outer if
+            }//end while
+        }//end for loop
+        return retList;
+    } //end method removeDuplicates
 
     public LinkedList<String> concatenateStrings() {
 
@@ -25,7 +65,12 @@ public class SuperLinkedList extends LinkedList<String> {
     }
 
     public String toString() {
-
-        return "";
-    }
+        ListIterator <String> iter = this.listIterator();
+        String retVal = iter.next();
+        while (iter.hasNext()){
+            retVal += ", ";
+            retVal += iter.next();
+        }//end while
+        return retVal;
+    }//end toString
 } // end SuperLinkedList
